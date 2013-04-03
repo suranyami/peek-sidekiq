c27efc666050c704c5a80a066a1df6fa314911f5
David Parry <david.parry@suranyami.com>
2013-04-03 15:38:08 +1100

    Now instrumenting Sidekiq::Client #push & #push_bulk.
    Based on how peek-pg does it.

--- lib/peek/views/sidekiq.rb
+++ lib/peek/views/sidekiq.rb
@@ -1,35 +1,67 @@
 require 'sidekiq'
+require 'atomic'
+
+class Sidekiq::Client
+  class << self
+    attr_accessor :query_time, :query_count
+  end
+
+  self.query_count = Atomic.new(0)
+  self.query_time = Atomic.new(0)
+
+  def push_with_timing(*args)
+    start = Time.now
+    push_without_timing(*args)
+  ensure
+    duration = (Time.now - start)
+    Sidekiq::Client.query_time.update { |value| value + duration }
+    Sidekiq::Client.query_count.update { |value| value + 1 }
+  end
+  alias_method_chain :push, :timing
+
+  def push_bulk_with_timing(*args)
+    start = Time.now
+    push_bulk_without_timing(*args)
+  ensure
+    duration = (Time.now - start)
+    Sidekiq::Client.query_time.update { |value| value + duration }
+    Sidekiq::Client.query_count.update { |value| value + 1 }
+  end
+end
 
 module Peek
   module Views
     class Sidekiq < View
-      def processed
-        stats.processed
+      def duration
+        ::Sidekiq::Client.query_time.value
       end
 
-      def failed
-        stats.failed
+      def formatted_duration
+        ms = duration * 1000
+        if ms >= 1000
+          "%.2fms" % ms
+        else
+          "%.0fms" % ms
+        end
       end
 
-      def enqueued
-        stats.enqueued
+      def calls
+        ::Sidekiq::Client.query_count.value
       end
 
       def results
-        {
-          processed: processed,
-          failed: failed,
-          enqueued: enqueued
-         }
+        {:duration => formatted_duration, :calls => calls}
       end
 
       private
-      def stats
-        @stats ||= ::Sidekiq::Stats.new
-        Rails.logger.debug @stats.processed
-        @stats
-      end
 
+      def setup_subscribers
+        # Reset each counter when a new request starts
+        before_request do
+          ::Sidekiq::Client.query_time.value = 0
+          ::Sidekiq::Client.query_count.value = 0
+        end
+      end
     end
   end
 end
